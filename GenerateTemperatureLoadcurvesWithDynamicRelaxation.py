2a56d18430c16d524025ef9f534b5c377fb52c8d
delta112358 <33021747+delta112358@users.noreply.github.com>
2018-01-23 09:57:45 +0100

    Update GenerateTemperatureLoadcurvesWithDynamicRelaxation.py

--- GenerateTemperatureLoadcurvesWithDynamicRelaxation.py
+++ GenerateTemperatureLoadcurvesWithDynamicRelaxation.py
@@ -32,7 +32,7 @@ def writeLoadCurves(loadFileName, relaxationTime, pulseTimes, roomTemperature, e
                 writeLoadCurve(loadFile, temperatureDifferenceCurve, roomTemperature, NodeNumber, timeVector)
 
 def writeLoadCurve(loadFile, temperatureDifferenceCurve, roomTemperature, NodeNumber, timeVector):
-    temperatureDifferenceCurve = [0] + [temperatureDifferenceCurve[0]] + [temperatureDifference for temperatureDifference in temperatureDifferenceCurve[1:] for _ in range(2)]
+    temperatureDifferenceCurve = [0] + [temperatureDifference for temperatureDifference in temperatureDifferenceCurve] + [temperatureDifferenceCurve[-1]]
     loadFile.write('*DEFINE_CURVE\n% 10.0f% 10.0f% 10.0f% 10.0f% 10.0f% 10.0f' % (NodeNumber,0,2,1,0,0))
     for timeIndex,time in enumerate(timeVector):
         try:
